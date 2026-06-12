010283ebc650b7c4afb69b75767b7ed8acc3e11c
digitalawarehub-prog <digitalawarehub@gmail.com>
2026-06-12 19:40:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = 
+    "Welcome to Digital Aware Hub";
+}
+function searchSite() {
+    let text =
+    document.getElementById("Search").value;
+    alert("You searched: " + text);
+}
