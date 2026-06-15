7ae5f9793ed237bc9392e5c4735579adc5beb344
digitalawarehub-prog <digitalawarehub@gmail.com>
2026-06-15 11:34:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function searchSite() {
     document.getElementById("Search").value;
     alert("You searched: " + text);
 }
+function searchPosts() {
+    let input = document.getElementById("searchInput").value.toLowerCase();
+    let posts = document.querySelectorAll(".card");
+    posts.forEach(post => {
+        let text = post.innerText.toLowerCase();
+        if (text.includes(input)) {
+            post.style.display = "block";
+        } else {
+            post.style.display = "none";
+        }
+    });
+}
+alert("Script Loaded");
