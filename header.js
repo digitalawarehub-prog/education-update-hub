00cbed177661658c243d11bb2098cfd1ba62159a
digitalawarehub-prog <digitalawarehub@gmail.com>
2026-07-08 22:40:16 +0530

    Add files via upload

--- header.js
+++ header.js
@@ -1,3 +1,4 @@
+document.getElementById("header").innerHTML = `
 <header>
 <div class="top-header">
 <div class="logo">
@@ -16,21 +17,21 @@
 </div>
 <nav class="navbar">
 <ul>
-<li><a href="index.html">Home</a></li>
+<li><a href="index.html">🏠 Home</a></li>
 <li><a href="latest-jobs.html">Latest Jobs</a></li>
 <li class="dropdown">
 <a href="#">Uttarakhand Jobs ▼</a>
 <ul class="dropdown-menu">
+<li><a href="uttarakhand-jobs.html">All Uttarakhand Jobs</a></li>
 <li><a href="ukpsc.html">UKPSC</a></li>
 <li><a href="uksssc.html">UKSSSC</a></li>
 <li><a href="high-court.html">High Court</a></li>
-<li><a href="forest.html">Forest</a></li>
-<li><a href="police.html">Police</a></li>
 </ul>
 </li>
 <li class="dropdown">
 <a href="#">Central Jobs ▼</a>
 <ul class="dropdown-menu">
+<li><a href="central-jobs.html">All Central Jobs</a></li>
 <li><a href="upsc.html">UPSC</a></li>
 <li><a href="ssc.html">SSC</a></li>
 <li><a href="banking-jobs.html">Banking</a></li>
@@ -40,6 +41,7 @@
 <li class="dropdown">
 <a href="#">Other State Jobs ▼</a>
 <ul class="dropdown-menu">
+<li><a href="other-state-jobs.html">All States</a></li>
 <li><a href="up-jobs.html">UP Jobs</a></li>
 <li><a href="bihar-jobs.html">Bihar Jobs</a></li>
 <li><a href="rajasthan-jobs.html">Rajasthan Jobs</a></li>
@@ -53,12 +55,12 @@
 <li><a href="utet.html">UTET</a></li>
 <li><a href="ugc-net.html">UGC NET</a></li>
 <li><a href="csir-ugc-net.html">CSIR UGC NET</a></li>
-<li><a href="cuet.html">CUET UG</a></li>
+<li><a href="cuet-ug.html">CUET UG</a></li>
 <li><a href="neet-ug.html">NEET UG</a></li>
 </ul>
 </li>
 <li><a href="admit-card.html">Admit Card</a></li>
-<li><a href="results.html">Results</a></li>
+<li><a href="result.html">Results</a></li>
 <li><a href="answer-key.html">Answer Key</a></li>
 <li><a href="syllabus.html">Syllabus</a></li>
 <li><a href="scholarship.html">Scholarship</a></li>
@@ -67,3 +69,4 @@
 </ul>
 </nav>
 </header>
+`;
