faffbfc25f3a7ffa3c932792e3c31c007dbea894
digitalawarehub-prog <digitalawarehub@gmail.com>
2026-07-24 00:01:11 +0530

    Add site configuration settings to settings.py

---
+++ bot/settings.py
@@ -0,0 +1,11 @@
+SITE_NAME = "Education Update Hub"
+SITE_URL = "https://educationupdatehub.in"
+
+ADSENSE_ID = "ca-pub-4508009805424675"
+GA4_ID = "G-XRESX2YP1N"
+
+DEFAULT_IMAGE = "https://educationupdatehub.in/images/logo.png"
+
+TWITTER_CARD = "summary_large_image"
+
+AUTHOR = "Education Update Hub"
