b827aeab54b3df252df8a6d5d59d6456ac175476
digitalawarehub-prog <digitalawarehub@gmail.com>
2026-07-23 23:23:06 +0530

    Filter and display new jobs from parsed jobs

--- bot/monitor.py
+++ bot/monitor.py
@@ -1,3 +1,13 @@
+from duplicate_checker import filter_new_jobs
+parsed_jobs = parse_jobs(all_jobs)
+
+new_jobs = filter_new_jobs(parsed_jobs)
+
+print(f"New Jobs : {len(new_jobs)}")
+
+for job in new_jobs:
+
+    print(job["title"])
 from source_manager import SourceManager
 from scraper import scrape_all_sources
 from parser import parse_jobs
