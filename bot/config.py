1cf9e89927cbb2bc276c361c93cad228627990ca
digitalawarehub-prog <digitalawarehub@gmail.com>
2026-07-23 22:12:56 +0530

    Add configuration for various government sources

---
+++ bot/config.py
@@ -0,0 +1,80 @@
+SOURCES = [
+
+# SSC
+{"name":"SSC","url":"https://ssc.gov.in"},
+
+# UPSC
+{"name":"UPSC","url":"https://upsc.gov.in"},
+
+# NTA
+{"name":"NTA","url":"https://nta.ac.in"},
+
+# Banking
+{"name":"IBPS","url":"https://www.ibps.in"},
+{"name":"SBI Careers","url":"https://sbi.co.in/web/careers"},
+{"name":"RBI","url":"https://opportunities.rbi.org.in"},
+{"name":"NABARD","url":"https://www.nabard.org/careers"},
+{"name":"LIC","url":"https://licindia.in/careers"},
+{"name":"SEBI","url":"https://www.sebi.gov.in"},
+
+# Railway
+{"name":"RRB Ahmedabad","url":"https://www.rrbahmedabad.gov.in"},
+{"name":"RRB Ajmer","url":"https://www.rrbajmer.gov.in"},
+{"name":"RRB Allahabad","url":"https://www.rrbald.gov.in"},
+{"name":"RRB Bangalore","url":"https://www.rrbbnc.gov.in"},
+{"name":"RRB Bhopal","url":"https://www.rrbbhopal.gov.in"},
+{"name":"RRB Bilaspur","url":"https://www.rrbbilaspur.gov.in"},
+{"name":"RRB Chandigarh","url":"https://www.rrbcdg.gov.in"},
+{"name":"RRB Chennai","url":"https://www.rrbchennai.gov.in"},
+{"name":"RRB Gorakhpur","url":"https://www.rrbgkp.gov.in"},
+{"name":"RRB Guwahati","url":"https://www.rrbguwahati.gov.in"},
+{"name":"RRB Jammu","url":"https://www.rrbjammu.nic.in"},
+{"name":"RRB Kolkata","url":"https://www.rrbkolkata.gov.in"},
+{"name":"RRB Malda","url":"https://www.rrbmalda.gov.in"},
+{"name":"RRB Mumbai","url":"https://www.rrbmumbai.gov.in"},
+{"name":"RRB Muzaffarpur","url":"https://www.rrbmuzaffarpur.gov.in"},
+{"name":"RRB Patna","url":"https://www.rrbpatna.gov.in"},
+{"name":"RRB Ranchi","url":"https://www.rrbranchi.gov.in"},
+{"name":"RRB Secunderabad","url":"https://www.rrbsecunderabad.gov.in"},
+{"name":"RRB Siliguri","url":"https://www.rrbsiliguri.gov.in"},
+{"name":"RRB Thiruvananthapuram","url":"https://www.rrbthiruvananthapuram.gov.in"},
+
+# Defence
+{"name":"Indian Army","url":"https://joinindianarmy.nic.in"},
+{"name":"Indian Navy","url":"https://www.joinindiannavy.gov.in"},
+{"name":"Indian Air Force","url":"https://agnipathvayu.cdac.in"},
+{"name":"Indian Coast Guard","url":"https://joinindiancoastguard.cdac.in"},
+{"name":"DRDO","url":"https://www.drdo.gov.in"},
+{"name":"ISRO","url":"https://www.isro.gov.in/Careers.html"},
+
+# Central Govt
+{"name":"ESIC","url":"https://www.esic.gov.in/recruitments"},
+{"name":"AIIMS Exams","url":"https://www.aiimsexams.ac.in"},
+{"name":"BSF","url":"https://rectt.bsf.gov.in"},
+{"name":"CRPF","url":"https://rect.crpf.gov.in"},
+{"name":"CISF","url":"https://cisfrectt.cisf.gov.in"},
+{"name":"ITBP","url":"https://recruitment.itbpolice.nic.in"},
+{"name":"SSB","url":"https://ssbrectt.gov.in"},
+{"name":"Delhi Police","url":"https://delhipolice.gov.in"},
+
+# PSUs
+{"name":"BHEL","url":"https://careers.bhel.in"},
+{"name":"ONGC","url":"https://ongcindia.com"},
+{"name":"IOCL","url":"https://iocl.com"},
+{"name":"HPCL","url":"https://hindustanpetroleum.com"},
+{"name":"BPCL","url":"https://bharatpetroleum.in"},
+{"name":"GAIL","url":"https://gailonline.com"},
+{"name":"SAIL","url":"https://sailcareers.com"},
+{"name":"NTPC","url":"https://careers.ntpc.co.in"},
+{"name":"NPCIL","url":"https://npcilcareers.co.in"},
+
+# Uttarakhand
+{"name":"UKPSC","url":"https://ukpsc.gov.in"},
+{"name":"UKPSC Online","url":"https://ukpsc.net.in"},
+{"name":"UKSSSC","url":"https://sssc.uk.gov.in"},
+{"name":"Uttarakhand High Court","url":"https://highcourtofuttarakhand.gov.in"},
+{"name":"Uttarakhand Police","url":"https://uttarakhandpolice.uk.gov.in"},
+{"name":"UBSE","url":"https://ubse.uk.gov.in"},
+{"name":"Uttarakhand Government","url":"https://uk.gov.in"}
+
+]
