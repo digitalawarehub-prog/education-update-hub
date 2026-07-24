064b08aadbeefb05da11e90ed33429203a82f65b
digitalawarehub-prog <digitalawarehub@gmail.com>
2026-07-24 18:31:19 +0530

    Add ADSENSE_CLIENT and GA_MEASUREMENT_ID variables

--- bot/config.py
+++ bot/config.py
@@ -17,7 +17,8 @@ DEFAULT_IMAGE = "https://educationupdatehub.in/images/default-job.jpg"
 ADSENSE_ID = "ca-pub-4508009805424675"
 
 GA4_ID = "G-XRESX2YP1N"
-
+ADSENSE_CLIENT = ADSENSE_ID
+GA_MEASUREMENT_ID = GA4_ID
 TWITTER_CARD = "summary_large_image"
 
 # ==========================================
