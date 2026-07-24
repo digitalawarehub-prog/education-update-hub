6016ab87f9adeed01032c48b0e2898cd0e208abe
digitalawarehub-prog <digitalawarehub@gmail.com>
2026-07-24 18:22:51 +0530

    Refactor SOURCES list for consistency

--- bot/config.py
+++ bot/config.py
@@ -1,33 +1,67 @@
+# ==========================================
+# Website
+# ==========================================
+
+SITE_NAME = "Education Update Hub"
+
+SITE_URL = "https://educationupdatehub.in"
+
+AUTHOR = "Education Update Hub"
+
+DEFAULT_IMAGE = "https://educationupdatehub.in/images/default-job.jpg"
+
+# ==========================================
+# Google
+# ==========================================
+
+ADSENSE_ID = "ca-pub-4508009805424675"
+
+GA4_ID = "G-XRESX2YP1N"
+
+TWITTER_CARD = "summary_large_image"
+
+# ==========================================
+# Social
+# ==========================================
+
+WHATSAPP_CHANNEL = "https://whatsapp.com/channel/0029Vb8LjDk6hENiaVSP4Q2a"
+
+TELEGRAM_CHANNEL = "https://t.me/YOUR_CHANNEL"
+
+# ==========================================
+# Sources
+# ==========================================
+
 SOURCES = [
 
-{
-    "name": "SSC",
-    "url": "https://ssc.gov.in",
-    "type": "html"
-},
+    {
+        "name": "SSC",
+        "url": "https://ssc.gov.in",
+        "type": "html"
+    },
 
-{
-    "name": "UPSC",
-    "url": "https://upsc.gov.in",
-    "type": "html"
-},
+    {
+        "name": "UPSC",
+        "url": "https://upsc.gov.in",
+        "type": "html"
+    },
 
-{
-    "name": "IBPS",
-    "url": "https://www.ibps.in",
-    "type": "html"
-},
+    {
+        "name": "IBPS",
+        "url": "https://www.ibps.in",
+        "type": "html"
+    },
 
-{
-    "name": "UKPSC",
-    "url": "https://ukpsc.gov.in",
-    "type": "html"
-},
+    {
+        "name": "UKPSC",
+        "url": "https://ukpsc.gov.in",
+        "type": "html"
+    },
 
-{
-    "name": "UKSSSC",
-    "url": "https://sssc.uk.gov.in",
-    "type": "html"
-}
+    {
+        "name": "UKSSSC",
+        "url": "https://sssc.uk.gov.in",
+        "type": "html"
+    }
 
 ]
