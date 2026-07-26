3508772f7c02fcbb50985750d7cdce2fd58206c5
digitalawarehub-prog <digitalawarehub@gmail.com>
2026-07-26 22:41:36 +0530

    Add pipeline.py with job processing functions

---
+++ bot/pipeline.py
@@ -0,0 +1,13 @@
+from database import (
+    load_jobs,
+    save_jobs
+)
+
+from optimizer import (
+    optimize_jobs,
+    add_timestamp,
+    merge_jobs,
+    filter_new_jobs
+)
+
+from summary import print_summary
