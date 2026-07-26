d2b3349a2173dbb7dfa36169bb171d4d6006ee0c
digitalawarehub-prog <digitalawarehub@gmail.com>
2026-07-26 21:31:12 +0530

    Add adapter imports and initialize ADAPTERS dictionary

--- bot/adapters/__init__.py
+++ bot/adapters/__init__.py
@@ -1,2 +1,26 @@
-# Education Update Hub
-# Adapter Package
+from .generic import GenericAdapter
+from .upsc import UPSCAdapter
+from .ssc import SSCAdapter
+from .ibps import IBPSAdapter
+from .railway import RailwayAdapter
+from .psc import PSCAdapter
+from .uk import UKAdapter
+
+
+ADAPTERS = {
+
+    "generic": GenericAdapter(),
+
+    "upsc": UPSCAdapter(),
+
+    "ssc": SSCAdapter(),
+
+    "ibps": IBPSAdapter(),
+
+    "railway": RailwayAdapter(),
+
+    "psc": PSCAdapter(),
+
+    "uk": UKAdapter()
+
+}
